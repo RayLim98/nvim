d36a5542c758b7810657d63111f7eebeb3ce5b83
Raymond Lim <rlim@securecodewarrior.com>
2025-08-23 22:25:34 +1000

    reformat

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,18 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { 'mason-org/mason.nvim', opts = {} },
+    'mason-org/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+
+    -- Useful status updates for LSP.
+    { 'j-hui/fidget.nvim', opts = {} },
+
+    -- Allows extra capabilities provided by blink.cmp
+    'saghen/blink.cmp',
+
+  },
+  config = function()
+    require "configs.lspconfig"
+  end,
+}
